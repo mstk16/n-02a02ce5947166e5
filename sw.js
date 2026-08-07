88b8ac4c6718713e6041c8d70243437d3e43d2cf
mstk16 <201199922+mstk16@users.noreply.github.com>
2026-08-08 01:01:11 +0900

    update egosearch view

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ろきじめ エゴサ ビューア — オフライン用 Service Worker
-const CACHE = 'roki-egosearch-v432b97405c8f';
+const CACHE = 'roki-egosearch-v7ef8dda9a939';
 const CORE = ['./', './index.html', './manifest.webmanifest',
               './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
